4615774d1e2edb263b2861fa7db528421cad2538
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2022-10-31 09:27:44 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+document.querySelector('.busca').addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  let input = document.querySelector('#searchInput').value;
+
+  if(input !== '') {
+    clearInfo()
+    showWarning('Loading...')
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=54571c872228726c0905d0d5728ca0c2&units=metric&lang=pt_br`
+
+    let results = await fetch(url)
+    let json = await results.json()
+
+    if(json.cod === 200) {
+      showInfo({
+        name: json.name,
+        country: json.sys.country,
+        temp: json.main.temp,
+        tempIcon: json.weather[0].icon,
+        windSpeed: json.wind.speed,
+        windAngle: json.wind.deg
+      })
+    } else {
+      clearInfo()
+      showWarning('We couldnt find this location.')
+    }
+  } else {
+    clearInfo()
+  }
+})
+
+function clearInfo() {
+  showWarning('')
+  document.querySelector('.resultado').style.display = 'none'
+}
+
+function showInfo(json) {
+  showWarning('')
+
+  document.querySelector('.resultado').style.display = 'block'
+
+  document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+  document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`
+  document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`
+
+  document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+  document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`
+
+}
+
+function showWarning(msg) {
+  document.querySelector('.aviso').innerHTML = msg;
+}
